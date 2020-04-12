340e47af46582009d28e7eba1ef965504e2b6f1a
Ian Edwards <ian.edward@uconn.edu>
2020-04-11 23:22:35 -0400

    update

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,22 @@
 const api = {
   getUser(username) {
+    curl https://api.github.com/users/defunkt
 
+    const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
   }
 };
 
