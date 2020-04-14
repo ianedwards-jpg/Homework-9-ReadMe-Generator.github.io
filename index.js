ae5e99c6ac73cc12a8384056f773ee9382ba73a6
Ian Edwards <ian.edward@uconn.edu>
2020-04-14 00:14:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const questions = [
-
+    {
+        name: 'faveReptile',
+        message: 'What is your favorite reptile?',
+      },
+      {
+        name: 'faveColor',
+        message: 'What is your favorite color?',
+      },
+      {
+        name: 'faveHeight',
+        message: 'How tall are you?',
+      },
 ];
 
 function writeToFile(fileName, data) {
